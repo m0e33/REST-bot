ad091988c037caf662620804206bdeed05b17f45
Moritz Spranger <moritz.spranger@gmail.com>
2021-05-22 23:20:37 +0200

    Formatting

--- data/csv_writer.py
+++ data/csv_writer.py
@@ -1,14 +1,15 @@
 import csv
 from typing import List
 
-class CSVWriter:
-  def __init__(self) -> None:
-      pass
 
-  def write(self, path: str, data, fieldnames: List[str]):
-    
-    with open(path, 'w', newline='') as csvfile:
-      writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=fieldnames)
-      writer.writeheader()
-      for entry in data:
-        writer.writerow(dict((k, entry[k]) for k in fieldnames))
+class CSVWriter:
+    def __init__(self) -> None:
+        pass
+
+    def write(self, path: str, data, fieldnames: List[str]):
+
+        with open(path, "w", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, delimiter=";", fieldnames=fieldnames)
+            writer.writeheader()
+            for entry in data:
+                writer.writerow(dict((k, entry[k]) for k in fieldnames))
