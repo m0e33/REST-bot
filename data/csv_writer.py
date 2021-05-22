6ef7587a4e097a6b3eaf46046689131e0d0f9d64
Moritz Spranger <moritz.spranger@gmail.com>
2021-05-22 12:06:13 +0200

    Implement get_historical_prices()

---
+++ data/csv_writer.py
@@ -0,0 +1,14 @@
+import csv
+from typing import List
+
+class CSVWriter:
+  def __init__(self) -> None:
+      pass
+
+  def write(self, path: str, data, fieldnames: List[str]):
+    
+    with open(path, 'w', newline='') as csvfile:
+      writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=fieldnames)
+      writer.writeheader()
+      for entry in data:
+        writer.writerow(dict((k, entry[k]) for k in fieldnames))
