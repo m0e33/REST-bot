abbf9d56579eea8a232026aae02180ec84980bd9
Moritz Spranger <moritz.spranger@gmail.com>
2021-05-29 11:24:40 +0200

    Linter

--- data/preprocesser.py
+++ data/preprocesser.py
@@ -1,7 +1,19 @@
+"""Preprocess data for model usage"""
+
 class Preprocessor:
+    """Preprocess data for model usage"""
     def __init__(self):
         pass
 
     def get_feedback_for_event(self):
+        """
+        Preprocess feedback data
+        """
+        pass
+
+    def public_method_for_linter(self):
+        """
+        Public method for linter
+        """
         pass
 
