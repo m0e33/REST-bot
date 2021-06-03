1f4d2a1d6bf8927748fe137099fbee426d0e2ca9
simonstadlinger <stadlinger.simon@gmail.com>
2021-06-03 09:58:14 +0200

    Fix caching in build() method

--- main_test_data.py
+++ main_test_data.py
@@ -6,5 +6,5 @@ from data.data_store import DataStore
 if __name__ == "__main__":
     symbols = ["AAPL", "ACN", "CDW", "NFLX"]
     data_store = DataStore(symbols, "2021-01-01", "2021-04-01")
-    data_store.rebuild()
+    data_store.build()
     print(data_store.get_price_data("AAPL"))
