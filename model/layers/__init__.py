00a0cafb7b657deab8419d5a055e34c511e17a98
simonstadlinger <stadlinger.simon@gmail.com>
2021-06-03 10:10:32 +0200

    Fix some linter errors

--- model/layers/__init__.py
+++ model/layers/__init__.py
@@ -1,7 +1,7 @@
 """Exposing all custom layer"""
 
-from word_event_embeddings import WordEventEmbeddings
-from event_information_encoder import EventInformationEncoder
-from stock_context_encoder import StockContextEncoder
-from stock_dependent_influence import StockDependentInfluence
-from stock_trend_forecaster import StockTrendForecaster
+from model.layers.word_event_embeddings import WordEventEmbeddings
+from model.layers.event_information_encoder import EventInformationEncoder
+from model.layers.stock_context_encoder import StockContextEncoder
+from model.layers.stock_dependent_influence import StockDependentInfluence
+from model.layers.stock_trend_forecaster import StockTrendForecaster
--- model/layers/stock_context_encoder.py
+++ model/layers/stock_context_encoder.py
@@ -6,7 +6,7 @@ from tensorflow import keras
 class StockContextEncoder(keras.layers.Layer):
     """Layer docstring"""
     def __init__(self):
-        super(StockContextEncoder, self).__init__()
+        super().__init__()
 
     def build(self, input_shape):
         """Gets executed, the first time the layer gets called"""
