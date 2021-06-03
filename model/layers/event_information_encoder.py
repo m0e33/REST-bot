df307bc0d91c2f909c6b0ef34eced28df8b6fd0d
simonstadlinger <stadlinger.simon@gmail.com>
2021-06-03 10:10:32 +0200

    Fix some linter errors

--- model/layers/event_information_encoder.py
+++ model/layers/event_information_encoder.py
@@ -6,7 +6,7 @@ from tensorflow import keras
 class EventInformationEncoder(keras.layers.Layer):
     """Layer docstring"""
     def __init__(self):
-        super(EventInformationEncoder, self).__init__()
+        super().__init__()
 
     def build(self, input_shape):
         """Gets executed, the first time the layer gets called"""
--- model/layers/word_event_embeddings.py
+++ model/layers/word_event_embeddings.py
@@ -7,7 +7,7 @@ class WordEventEmbeddings(keras.layers.Layer):
     """Layer docstring"""
 
     def __init__(self, embedding_dim=100):
-        super(WordEventEmbeddings, self).__init__()
+        super().__init__()
         self.embedding_dim = embedding_dim
 
     def build(self, input_shape):
