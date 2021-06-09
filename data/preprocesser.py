f4103974d52f2f513711dc01614b4c01b68348d8
simonstadlinger <stadlinger.simon@gmail.com>
2021-06-10 00:06:12 +0200

    #27 Add support for train, test, val split, Add train_configuration, Make Linter happy

--- data/preprocesser.py
+++ data/preprocesser.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 from data.data_store import DataStore
 from data.data_configuration import DataConfiguration
 from data.data_info import PriceDataInfo
+from model.train_configuration import TrainConfiguration
 
 
 class EventType(Enum):
@@ -34,27 +35,38 @@ class Preprocessor:
 
     """Preprocess data for model usage"""
 
-    def __init__(self, data_store: DataStore, data_cfg: DataConfiguration):
+    # pylint: disable=too-many-instance-attributes
+    def __init__(
+        self,
+        data_store: DataStore,
+        data_cfg: DataConfiguration,
+        train_cfg: TrainConfiguration,
+    ):
         self.data_store = data_store
         self.data_cfg = data_cfg
+        self.train_cfg = train_cfg
         self.date_df = self._build_date_dataframe()
-        self._events_df = pd.DataFrame()
-        self._gt_df = pd.DataFrame()
+
+        # Predefine all dataframes for linter._.
+        self._events_train_df = pd.DataFrame()
+        self._gt_train_df = pd.DataFrame()
+        self._events_val_df = pd.DataFrame()
+        self._gt_val_df = pd.DataFrame()
+        self._events_test_df = pd.DataFrame()
+        self._gt_test_df = pd.DataFrame()
 
     def build_events_data_with_gt(self):
         """builds event data"""
 
         # vertically concatenate all symbols and their events
-        self._events_df = pd.concat(
+        events_df = pd.concat(
             [self._build_df_for_symbol(symbol) for symbol in self.data_cfg.symbols]
         )
 
         # join event_title & event_text columns
-        self._events_df["event"] = (
-            self._events_df["event_type"] + " " + self._events_df["event_text"]
-        )
-        self._events_df = self._events_df.drop(["event_type", "event_text"], axis=1)
-        self._events_df = self._events_df.astype({"event": object})
+        events_df["event"] = events_df["event_type"] + " " + events_df["event_text"]
+        events_df = events_df.drop(["event_type", "event_text"], axis=1)
+        events_df = events_df.astype({"event": object})
 
         # build multi-index dataframe per date and symbol to later generate tensors
         # with the right shape easily
@@ -62,29 +74,42 @@ class Preprocessor:
         # The grouping with gt_trend is unnecessary here, because it holds the same grouping
         # information as 'date' and 'symbol' combined. We have to list it here in order
         # to copy it over to the new events_df dataframe
-        self._events_df = (
-            self._events_df.groupby(["date", "symbol", "gt_trend"])["event"]
+        events_df = (
+            events_df.groupby(["date", "symbol", "gt_trend"])["event"]
             .apply(list)
             .reset_index()
         )
-        self._events_df.set_index(["date", "symbol"], inplace=True)
-        self._events_df.index = self._events_df.index.set_levels(
-            self._events_df.index.levels[0].date, level=0
+        events_df.set_index(["date", "symbol"], inplace=True)
+        events_df.index = events_df.index.set_levels(
+            events_df.index.levels[0].date, level=0
         )
 
-        self._gt_df = self._events_df["gt_trend"]
-        self._events_df = self._events_df["event"]
+        self._set_train_val_test_split(events_df)
 
-    def get_tf_dataset(self):
-        """Return windowed dataset for model based on events_df"""
+    def get_train_ds(self):
+        """windowed tensorflow training dataset"""
+        return self._get_tf_dataset(self._events_train_df, self._gt_train_df)
+
+    def get_val_ds(self):
+        """windowed tensorflow validation dataset"""
+        return self._get_tf_dataset(self._events_val_df, self._gt_val_df)
+
+    def get_test_ds(self):
+        """windowed tensorflow validation dataset"""
+        return self._get_tf_dataset(self._events_test_df, self._gt_test_df)
+
+    def _get_tf_dataset(self, events_df, gt_df):
+        """Return windowed dataset based on events_df and ground truth"""
 
         sliding_window_length = self.data_cfg.stock_context_days
 
-        dates_count = len(self._events_df.index.levels[0])
-        symbols_count = len(self._events_df.index.levels[1])
+        dates_count = len(events_df.groupby(level=0))
+        symbols_count = len(events_df.groupby(level=1))
 
         # build the input tensor
-        np_stock_matrix = self._events_df.values.reshape(dates_count, symbols_count, 1)
+        # we have to use ragged tensor here, since we don't know how many events
+        # per date and symbol there are
+        np_stock_matrix = events_df.values.reshape(dates_count, symbols_count, 1)
         events_ragged_t = tf.ragged.constant(np_stock_matrix)
         events_ragged_t = tf.squeeze(events_ragged_t, axis=[2])
 
@@ -94,7 +119,7 @@ class Preprocessor:
         events_t = events_ragged_t.to_tensor()
 
         # build the output tensor
-        np_gt_trend_matrix = self._gt_df.values.reshape(dates_count, symbols_count, 1)
+        np_gt_trend_matrix = gt_df.values.reshape(dates_count, symbols_count, 1)
 
         # since the 'timeseries_dataset_from_array' documentation states:
         #
@@ -191,3 +216,35 @@ class Preprocessor:
         return symbol_price_df.drop(
             [field for field in PriceDataInfo.fields if field != "date"], axis=1
         )
+
+    def _set_train_val_test_split(self, events_df):
+
+        actual_val_split = 1 - (self.train_cfg.val_split + self.train_cfg.test_split)
+        actual_test_split = 1 - self.train_cfg.test_split
+
+        # since np.split does not take hierarchical indexing into account
+        # but rather flattens the index we have to make sure not to split
+        # in the middle of a day
+        dates_count = len(events_df.index.levels[0])
+        symbols_count = len(events_df.index.levels[1])
+
+        dates_val_split = int(dates_count * actual_val_split) * symbols_count
+        dates_test_split = int(dates_count * actual_test_split) * symbols_count
+
+        # pylint: disable=unbalanced-tuple-unpacking
+        events_train_df, events_val_df, events_test_df = np.split(
+            events_df,
+            [
+                dates_val_split,
+                dates_test_split,
+            ],
+        )
+
+        self._events_train_df = events_train_df["event"]
+        self._gt_train_df = events_train_df["gt_trend"]
+
+        self._events_val_df = events_val_df["event"]
+        self._gt_val_df = events_val_df["gt_trend"]
+
+        self._events_test_df = events_test_df["event"]
+        self._gt_test_df = events_test_df["gt_trend"]
