0605636ca193823da39586557edcdb1ff7ea5747
simonstadlinger <stadlinger.simon@gmail.com>
2021-06-30 14:05:11 +0200

    #31 Introduce hyperparameter configuration and add type specific encoder to RESTNet

--- model/model.py
+++ model/model.py
@@ -7,20 +7,21 @@ from model.layers import \
     StockDependentInfluence, \
     StockTrendForecaster, \
     TypeSpecificEncoder
-
+from model.configuration import HyperParameterConfiguration
 
 class RESTNet(keras.Model):
     """Architecture for stock trend prediction"""
-    def __init__(self):
+    def __init__(self, hp_cfg: HyperParameterConfiguration):
         super(RESTNet, self).__init__()
 
+        # parameter
+        self.hp_cfg = hp_cfg
+
         # model architecture
-        # trainable
-        self.type_specific_encoder = TypeSpecificEncoder()
-        self.event_information_encoder = EventInformationEncoder()
+        self.type_specific_encoder = TypeSpecificEncoder(self.hp_cfg.attn_cnt)
         self.stock_context_encoder = StockContextEncoder()
         self.stock_dependent_influence = StockDependentInfluence()
         self.stock_trend_forecaster = StockTrendForecaster()
 
     def call(self, inputs):
-        pass
+        return self.type_specific_encoder.call(inputs)
