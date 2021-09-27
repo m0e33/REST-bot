913d5ae5ac218e7b0f20ce4bda4b5b1840a275d1
simonstadlinger <stadlinger.simon@gmail.com>
2021-09-27 12:50:58 +0200

    Add limitless to symbol loading

--- utils/symbols.py
+++ utils/symbols.py
@@ -4,4 +4,8 @@ import yaml
 def load_symbols(limit):
     with open("symbols.yaml", 'r') as stream:
         symbols = yaml.safe_load(stream)
-    return list(symbols['symbols'].keys())[:limit]
+
+    if limit is None:
+        return list(symbols['symbols'].keys())
+    else:
+        return list(symbols['symbols'].keys())[:limit]
